f934b7ebc8335ae3302cb223acffa1ca3d84cad2
nabishafin <shafin21215@gmail.com>
2024-12-27 16:34:50 +0600

    some data update

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ require('dotenv').config()
 const port = process.env.PORT || 5000
 const app = express()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-app.use(cors());
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 app.use(express.json())
 
 
